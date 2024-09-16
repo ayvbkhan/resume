ddae0d8a0e25b56937c718cc5a61247f7e81ec07
ayvbkhan <malcolmx2507@gmail.com>
2024-09-16 15:00:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let username = "Ayubkhan"
+username = "Meliboev Ayubkhan"
+console.log(username)
+
+let a = 10
+let b = 20 
+
+let c = a + username 
+
+let message = "Привет, " + username + "!"
+message = `Привет, ${username}!`
+
+console.log(message) 
+
+console.log(c)
